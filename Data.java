d8d9de98b3a0c826bdb6595f781002d44fd8d376
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2026-04-07 13:24:13 +0530

    data type

---
+++ Data.java
@@ -0,0 +1,13 @@
+class Data{
+    public static void main(String[]args){
+        int age=20;
+        Float num=20.2f;
+        double price=225.99;
+
+        System.out.println(age);
+        System.out.println(num);
+        System.out.println(price);
+
+
+    }
+}
